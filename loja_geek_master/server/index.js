57836da7f6c726f693c46643d0491ade8c91377e
williamlopesdev <wilcypherr@gmail.com>
2021-02-11 20:34:50 -0300

    modificar um módulo ou uma função do seu sistema para que funcione com SSR - Side Server Rendering.

---
+++ loja_geek_master/server/index.js
@@ -0,0 +1,50 @@
+import path from 'path';
+
+import fs from 'fs';
+
+import React from 'react';
+
+import express from 'express';
+
+import ReactDOMServer from 'react-dom/server';
+
+//side server é o code spliting Unidades
+import Unidades from '../src/code_spliting/Unidades';
+
+const PORT = process.env.PORT || 3006;
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const app = ReactDOMServer.renderToString(<Unidades />);
+
+  const indexFile = path.resolve('./build/index.html');
+
+   fs.readFile(indexFile, 'utf8', (err, data) => {
+
+    if (err) {
+
+       console.error('Something went wrong:', err);
+
+      return res.status(500).send('Oops, better luck next time!');
+
+    }
+
+    return res.send(
+
+       data.replace('<div id="root"></div>', `<div id="root">${Unidades}</div>`)
+
+    );
+
+  });
+
+});
+
+app.use(express.static('./build'));
+
+app.listen(PORT, () => {
+
+  console.log(`Server is listening on port ${PORT}`);
+
+});
